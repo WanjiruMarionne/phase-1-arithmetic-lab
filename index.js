e6c5eae029f49ec9df144434f3cfddccd0a17434
Wanjiru Mburu <wanjirumarionne@gmail.com>
2023-12-15 15:41:05 +0300

    Completed Arithmetics Lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 1
+const num2 = 62
+const multiply = num1*num2
 
-// Write your code here
+const random = Math.ceil(Math.random()) * Number.MAX_SAFE_INTEGER;
+
+const num3 = 40
+const num4 = 9
+const mod = num3 % num4
+
+const max = Math.max(4,10,20,8,12)
